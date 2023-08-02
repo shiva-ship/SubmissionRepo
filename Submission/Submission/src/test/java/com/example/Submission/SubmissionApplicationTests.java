8b646acc5ad3e42bf43aedd926cba2b351ee6ca7
NIKHIL SONTAKKE <nikhilsontakke111@gmail.com>
2023-08-02 14:01:01 +0530

    Initial Commit
    Initital Commit

---
+++ Submission/Submission/src/test/java/com/example/Submission/SubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Submission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
